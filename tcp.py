41edb076ffd52fe3eca8552f95eb5463ce069dab
lututui <lututuibr@gmail.com>
2021-10-09 15:25:55 -0300

    Testes 1 e 2

--- tcp.py
+++ tcp.py
@@ -18,8 +18,7 @@ class Servidor:
         self.callback = callback
 
     def _rdt_rcv(self, src_addr, dst_addr, segment):
-        src_port, dst_port, seq_no, ack_no, \
-            flags, window_size, checksum, urg_ptr = read_header(segment)
+        src_port, dst_port, seq_no, ack_no, flags, window_size, checksum, urg_ptr = read_header(segment)
 
         if dst_port != self.porta:
             # Ignora segmentos que não são destinados à porta do nosso servidor
@@ -28,13 +27,13 @@ class Servidor:
             print('descartando segmento com checksum incorreto')
             return
 
-        payload = segment[4*(flags>>12):]
+        payload = segment[4 * (flags >> 12):]
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
             if self.callback:
@@ -48,22 +47,45 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seq_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
-        self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
-        #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+        self.seq_no = seq_no
+
+        self._handshake()
+
+        self.servidor.callback(self)
+
+        # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
+        # self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)
+
+        # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+        # self.timer.cancel()
 
     def _exemplo_timer(self):
         # Esta função é só um exemplo e pode ser removida
         print('Este é um exemplo de como fazer um timer')
 
+    def _make_header(self, bytes_len, flags):
+        return make_header(self.id_conexao[3], self.id_conexao[1], self.seq_no, self.seq_no + bytes_len, flags)
+
+    def _handshake(self):
+        self.enviar(self._make_header(1, FLAGS_ACK | FLAGS_SYN))
+
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        print('recebido payload: %r' % payload)
+        print(f'expected seq: {self.seq_no + 1}, got: {seq_no}')
+        if self.seq_no + 1 == seq_no:
+            print("RECEIVED CORRECT SEQ_NO")
+            self.seq_no += len(payload)
+            self.enviar(self._make_header(1, FLAGS_ACK))
+
+            print(f'recebido payload len: {len(payload)}, seq_no: {seq_no}, ack_no: {ack_no}')
+            print('%r' % payload)
+            self.callback(self.id_conexao, payload)
 
     # Os métodos abaixo fazem parte da API
 
@@ -78,10 +100,7 @@ class Conexao:
         """
         Usado pela camada de aplicação para enviar dados
         """
-        # TODO: implemente aqui o envio de dados.
-        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
-        # que você construir para a camada de rede.
-        pass
+        self.servidor.rede.enviar(fix_checksum(dados, self.id_conexao[0], self.id_conexao[2]), self.id_conexao[0])
 
     def fechar(self):
         """
